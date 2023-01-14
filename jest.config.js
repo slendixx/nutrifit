0d33914ef1ddbc5e437b8fe8b6a0a57f705747e9
Esteban <esteban321duran@gmail.com>
2023-01-13 23:20:05 -0300

    Added configuration for using global path alias in typescript and jest.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 const nextJest = require('next/jest');
+const tsconfig = require("./tsconfig.json")
+const moduleNameMapper = require("tsconfig-paths-jest")(tsconfig)
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test
@@ -9,13 +11,16 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
-  },
+  moduleNameMapper,
+  // moduleNameMapper: {
+  //   // Handle module aliases (this will be automatically configured for you soon)
+  //   '^@/components/(.*)$': '<rootDir>/components/$1',
+  //   '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  // },
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: [
+      "<rootdir>/__tests__/__mocks__/"
+  ]
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which
--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,6 @@
 // Optional: configure or set up a testing framework before each test.
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
-import { server } from './__tests__/mocks/msw/server';
+import { server } from './__tests__/__mocks__/msw/server';
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
